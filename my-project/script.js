7052d46088755c3eb6822012fdb4a1d83513fed2
Ismail-Heasat <esmailheiasat2004@gmail.com>
2023-11-28 13:50:52 +0300

    Your commit message here

--- my-project/script.js
+++ my-project/script.js
@@ -93,7 +93,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://savge.onrender.comc', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
